42414f1884593a4f80ae34844dbcf3105673496e
evelina <ina.nyman98@gmail.com>
2025-12-22 15:46:18 +0100

    added some basic styling to navbar that we will make nicer when we move to frontend

--- eshop-soulcaller-frontend/src/components/Navbar/Navbar.jsx
+++ eshop-soulcaller-frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Navbar.scss'
 
-const Navbar = () => {
+export default function Navbar() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div>Navbar</div>
+    <header className="navbar">
+      <div className="navbar__container">
+        <a className="navbar__logo" href="/">Soulcaller</a>
+
+        <button
+          className={`navbar__toggle ${open ? 'open' : ''}`}
+          aria-label="Toggle menu"
+          onClick={() => setOpen(!open)}
+        >
+          <span className="bar" />
+          <span className="bar" />
+          <span className="bar" />
+        </button>
+
+        <nav className={`navbar__nav ${open ? 'open' : ''}`}>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#shop">Shop</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
-
-export default Navbar
