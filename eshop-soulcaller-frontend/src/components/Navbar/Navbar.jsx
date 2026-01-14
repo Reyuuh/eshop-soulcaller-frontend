98c27b825dbac303c79685dc503fa2321d292029
evelina <ina.nyman98@gmail.com>
2026-01-14 16:51:12 +0100

    updated links in navbar to actual links that that us to our pages

--- eshop-soulcaller-frontend/src/components/Navbar/Navbar.jsx
+++ eshop-soulcaller-frontend/src/components/Navbar/Navbar.jsx
@@ -23,10 +23,10 @@ export default function Navbar() {
 
         <nav className={`navbar__nav ${open ? 'open' : ''}`}>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#shop">Shop</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Register User</a></li>
+            <li><a href="/admin">Admin</a></li>
           </ul>
         </nav>
 
